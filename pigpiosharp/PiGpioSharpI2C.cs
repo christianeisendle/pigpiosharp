e4297613208e59674c38d0e91193a1e88e1c5f92
Christian Eisendle <christian@eisendle.net>
2017-09-15 22:24:04 +0200

    Add writeDevice API for I2C class

--- pigpiosharp/PiGpioSharpI2C.cs
+++ pigpiosharp/PiGpioSharpI2C.cs
@@ -1,44 +1,49 @@
 ﻿using System;
 namespace PiGpio
 {
-	public class I2C
-	{
-#region Commands
-		private const int I2CO = 54;
-		private const int I2CC = 55;
-		private const int I2CRD = 56;
-		private const int I2CWD = 57;
-		private const int I2CWQ = 58;
-		private const int I2CRS = 59;
-		private const int I2CWS = 60;
-		private const int I2CRB = 61;
-		private const int I2CWB = 62;
-		private const int I2CRW = 63;
-		private const int I2CWW = 64;
-		private const int I2CRK = 65;
-		private const int I2CWK = 66;
-		private const int I2CRI = 67;
-		private const int I2CWI = 68;
-		private const int I2CPC = 69;
-		private const int I2CPK = 70;
-#endregion
+    public class I2C
+    {
+        #region Commands
+        private const int I2CO = 54;
+        private const int I2CC = 55;
+        private const int I2CRD = 56;
+        private const int I2CWD = 57;
+        private const int I2CWQ = 58;
+        private const int I2CRS = 59;
+        private const int I2CWS = 60;
+        private const int I2CRB = 61;
+        private const int I2CWB = 62;
+        private const int I2CRW = 63;
+        private const int I2CWW = 64;
+        private const int I2CRK = 65;
+        private const int I2CWK = 66;
+        private const int I2CRI = 67;
+        private const int I2CWI = 68;
+        private const int I2CPC = 69;
+        private const int I2CPK = 70;
+        #endregion
 
-		private PiGpioSharp m_pi;
-		private int m_handle;
+        private PiGpioSharp m_pi;
+        private int m_handle;
 
-		public I2C(PiGpioSharp pi)
-		{
-			m_pi = pi;
-		}
+        public I2C(PiGpioSharp pi)
+        {
+            m_pi = pi;
+        }
 
-		public void open(int bus, int address, int i2c_flags = 0)
-		{
-			m_handle = m_pi.executeCommand(I2CO, bus, address, i2c_flags);
-		}
+        public void open(int bus, int address, int i2c_flags = 0)
+        {
+            m_handle = m_pi.executeCommand(I2CO, bus, address, i2c_flags);
+        }
 
-		public void close()
-		{
-			m_pi.executeCommand(I2CC, m_handle, 0);
-		}
-	}
+        public void close()
+        {
+            m_pi.executeCommand(I2CC, m_handle, 0);
+        }
+
+        public void writeDevice(byte[] data)
+        {
+            m_pi.executeCommand(I2CWD, m_handle, 0, data.Length, data);
+        }
+    }
 }
--- pigpiosharp/test/PiGpioI2CUnitTest.cs
+++ pigpiosharp/test/PiGpioI2CUnitTest.cs
@@ -9,6 +9,11 @@ namespace PiGpio.Test
         private static PiGpioSharp pi;
         private static I2C i2c;
 
+        private static void open(int slaveAddr)
+        {
+            i2c.open(1, slaveAddr);
+        }
+
         [OneTimeSetUp]
         public static void setup()
         {
@@ -19,8 +24,7 @@ namespace PiGpio.Test
         [Test, Order(1)]
         public static void open()
         {
-            int busNumber = 1;
-            i2c.open(busNumber, 0x28);
+            open(0x28);
         }
 
         [Test, Order(2)]
@@ -33,6 +37,14 @@ namespace PiGpio.Test
         public static void openBadI2CBus()
         {
             Assert.That(() => i2c.open(10, 0x28), Throws.Exception.TypeOf<CommandFailedException>().With.Property("Error").EqualTo(ErrorCode.PI_BAD_I2C_BUS));
+        }
+
+        [Test]
+        public static void writeDeviceToNonExistingDevice()
+        {
+            byte[] tmp = { 0, 1, 2 };
+            open(0x30);
+            Assert.That(() => i2c.writeDevice(tmp), Throws.Exception.TypeOf<CommandFailedException>().With.Property("Error").EqualTo(ErrorCode.PI_I2C_WRITE_FAILED));
         }
     }
 }
