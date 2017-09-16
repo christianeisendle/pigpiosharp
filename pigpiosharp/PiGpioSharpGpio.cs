b0d743e3e44e748aae28ab5f62c150cbd1386973
Christian Eisendle <christian@eisendle.net>
2017-09-16 22:49:02 +0200

    Add GPIO functions

---
+++ pigpiosharp/PiGpioSharpGpio.cs
@@ -0,0 +1,57 @@
+﻿namespace PiGpio
+{
+    public enum GpioMode
+    {
+        INPUT = 0,
+        OUTPUT = 1,
+        ALT0 = 4,
+        ALT1 = 5,
+        ALT2 = 6,
+        ALT3 = 7,
+        ALT4 = 3,
+        ALT5 = 2
+    }
+
+    public enum GpioPullUpDown
+    {
+        PUD_OFF = 0,
+        PUD_DOWN = 1,
+        PUD_UP = 2
+    }
+
+    public class Gpio
+    {
+        readonly PiGpioSharp m_pi;
+        int m_handle;
+
+        public Gpio(PiGpioSharp pi)
+        {
+            m_pi = pi;
+        }
+
+        public void SetMode(int gpioNum, GpioMode mode)
+        {
+            m_handle = m_pi.ExecuteCommand(CommandCode.PI_CMD_MODES, gpioNum, (int)mode);
+        }
+
+        public GpioMode GetMode(int gpioNum)
+        {
+            return (GpioMode)m_pi.ExecuteCommand(CommandCode.PI_CMD_MODEG, gpioNum, 0);
+        }
+
+        public void SetPullUpPullDown(int gpioNum, GpioPullUpDown pud)
+        {
+            m_pi.ExecuteCommand(CommandCode.PI_CMD_PUD, gpioNum, (int)pud);
+        }
+
+        public int Read(int gpioNum)
+        {
+            return m_pi.ExecuteCommand(CommandCode.PI_CMD_READ, gpioNum, 0);
+        }
+
+        public void Write(int gpioNum, int value)
+        {
+            m_pi.ExecuteCommand(CommandCode.PI_CMD_PUD, gpioNum, value);
+        }
+    }
+}
---
+++ pigpiosharp_test/PiGpioUnitTest.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+
+namespace PiGpio.Test
+{
+    public static class PiGpioI2CUnitTest
+    {
+        static PiGpioSharp pi;
+        static I2C i2c;
+
+        static void Open(int slaveAddr)
+        {
+            i2c.Open(1, slaveAddr);
+        }
+
+        [OneTimeSetUp]
+        public static void Setup()
+        {
+            pi = new PiGpioSharp("raspi", 8888);
+            i2c = new I2C(pi);
+        }
+
+        [Test, Order(1)]
+        public static void Open()
+        {
+            Open(0x28);
+        }
+
+        [Test, Order(2)]
+        public static void Close()
+        {
+            i2c.Close();
+        }
+
+        [Test]
+        public static void OpenBadI2CBus()
+        {
+            Assert.That(() => i2c.Open(10, 0x28), Throws.Exception.TypeOf<CommandFailedException>().With.Property("Error").EqualTo(ErrorCode.PI_BAD_I2C_BUS));
+        }
+
+        [Test]
+        public static void WriteDeviceToNonExistingDevice()
+        {
+            byte[] tmp = { 0, 1, 2 };
+            Open(0x30);
+            Assert.That(() => i2c.WriteDevice(tmp), Throws.Exception.TypeOf<CommandFailedException>().With.Property("Error").EqualTo(ErrorCode.PI_I2C_WRITE_FAILED));
+        }
+
+        [Test]
+        public static void ReadDeviceToNonExistingDevice()
+        {
+            Open(0x30);
+            Assert.That(() => i2c.ReadDevice(10), Throws.Exception.TypeOf<CommandFailedException>().With.Property("Error").EqualTo(ErrorCode.PI_I2C_READ_FAILED));
+            Close();
+        }
+
+        [Test]
+        public static void ReadDevice()
+        {
+            Open(0x28);
+            i2c.ReadDevice(10);
+            Close();
+        }
+    }
+}
